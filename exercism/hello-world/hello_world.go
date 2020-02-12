9797aa5c19456789fb6803f5bd3da23661d21350
Ozan Ürkmez <arkantos250@gmail.com>
2020-02-12 10:55:32 +0300

    solve hello world

---
+++ exercism/hello-world/hello_world.go
@@ -0,0 +1,7 @@
+// Package greeting contains the solution to the hello world problem.
+package greeting
+
+// HelloWorld prints a greeting to za warudo.
+func HelloWorld() string {
+	return "Hello, World!"
+}
