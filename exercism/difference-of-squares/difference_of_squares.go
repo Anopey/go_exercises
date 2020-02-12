f69b9d0f9dd18116d843b0286c15eba5ad7c96ff
Ozan Ürkmez <arkantos250@gmail.com>
2020-02-12 15:45:36 +0300

    solve difference of squares.

--- exercism/difference-of-squares/difference_of_squares.go
+++ exercism/difference-of-squares/difference_of_squares.go
@@ -1,1 +1,20 @@
 package diffsquares
+
+//SquareOfSum calculates the square of the sum of all the first n natural numbers
+func SquareOfSum(n int) int {
+	var sum int = 0
+	for i := 1; i <= n; i++ {
+		sum += i
+	}
+	return sum * sum
+}
+
+//SumOfSquares takes the sum of the squares of all of the first n natural numbers
+func SumOfSquares(n int) int {
+	return n * (n + 1) * (2*n + 1) / 6
+}
+
+//Difference takes the difference of SquareOfSum and SumOfSquares of the input number.
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
+}
