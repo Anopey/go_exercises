7a4ab08768c3ebc7dd4588f3289df0771ce57bd5
Ozan Ürkmez <arkantos250@gmail.com>
2020-02-12 14:09:54 +0300

    alternate solution

--- exercism/reverse-string/reverse_string.go
+++ exercism/reverse-string/reverse_string.go
@@ -3,10 +3,10 @@ package reverse
 
 //Reverse function reverses a given string
 func Reverse(inp string) string {
-	var output []rune
+	var output string
 	runes := []rune(inp)
 	for i := len(runes) - 1; i >= 0; i-- {
-		output = append(output, runes[i])
+		output += string(runes[i])
 	}
-	return string(output)
+	return output
 }
