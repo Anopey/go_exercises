d73ace22b491cf597001f6ae99153fad8d1e8815
Ozan Ürkmez <arkantos250@gmail.com>
2020-02-13 15:01:31 +0300

    solve grains

---
+++ exercism/grains/grains.go
@@ -0,0 +1,24 @@
+package grains
+
+import "errors"
+
+//Square takes two to the power of i
+func Square(i int) (uint64, error) {
+	if i < 1 || i > 64 {
+		return 0, errors.New("aaaa")
+	}
+	return uint64(1) << (i - 1), nil
+}
+
+//Total takes total of grains.
+func Total() uint64 {
+	var total uint64 = 0
+	for i := 1; i <= 64; i++ {
+		v, e := Square(i)
+		if e != nil {
+			panic("You lied to me!")
+		}
+		total += v
+	}
+	return total
+}
