243a4e8a2147d582c4d7b7c2f2d3c30076761d0e
Ozan Ürkmez <arkantos250@gmail.com>
2020-02-19 11:28:55 +0300

    solve isogram

--- exercism/isogram/isogram.go
+++ exercism/isogram/isogram.go
@@ -1,1 +1,23 @@
 package isogram
+
+import (
+	"strings"
+	"unicode"
+)
+
+//IsIsogram evalues whether the given string is an isogram or not.
+func IsIsogram(s string) bool {
+	chars := make(map[rune]int)
+	s = strings.ToUpper(s)
+	for _, v := range s {
+		if !unicode.IsLetter(v) {
+			continue
+		}
+		if _, ok := chars[v]; ok {
+			return false
+		} else {
+			chars[v] = 0
+		}
+	}
+	return true
+}
