3ccf58486d1d966e70f32443edd13bdb74cec7b6
Ozan Ürkmez <arkantos250@gmail.com>
2020-02-19 11:20:03 +0300

    upload isogram question

--- exercism/grains/grains.go
+++ exercism/grains/grains.go
@@ -5,7 +5,7 @@ import "errors"
 //Square takes two to the power of i
 func Square(i int) (uint64, error) {
 	if i < 1 || i > 64 {
-		return 0, errors.New("aaaa")
+		return 0, errors.New("integer square out of bounds or not able to be squared.")
 	}
 	return uint64(1) << (i - 1), nil
 }
@@ -16,7 +16,7 @@ func Total() uint64 {
 	for i := 1; i <= 64; i++ {
 		v, e := Square(i)
 		if e != nil {
-			panic("You lied to me!")
+			panic("Square function stopped working.")
 		}
 		total += v
 	}
